8f23a0fd7c8513096e191a7501dc9454322e24e1
lawson200 <melchisedec2000@gmail.com>
2024-09-16 17:22:28 +0200

    Fonction sans opérateur

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,13 +1,24 @@
 
-def puissance (a,b):
-	return a**b
-
-# fonctions.py
 
 def puissance(a, b):
+
 
     if not isinstance(a, int) or not isinstance(b, int):
         raise TypeError("Only integers are allowed for both arguments.")
 
-    return a ** b
+    if b == 0:
+        return 1
+    negatif = False
+    if b < 0:
+        negatif = True
+        b = -b
+
+    resultat = 1
+    for _ in range(b):
+        resultat *= a
+
+    if negatif:
+        return 1 / resultat
+    
+    return resultat
 
